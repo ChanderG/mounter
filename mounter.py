34d4e16343f5d74053dd4ba35255d735d9cdf025
ChanderG <chandergovind@gmail.com>
2015-12-26 09:38:22 +0530

    add lsblk command output

--- mounter.py
+++ mounter.py
@@ -1,16 +1,32 @@
 """ CLI tool to mount/umount devices."""
 import urwid
+import subprocess
 
 def handle_top_inputs(key):
     if key == 'q':
         raise urwid.ExitMainLoop()
+
+device_screen = None
+
+def getDeviceScreen():
+    """ Return the device screen."""
+    global device_screen
+    if device_screen != None:
+        return device_screen
+
+    # actual setup
+    title = urwid.Text("Select device: ", 'center')
+    contents_ = subprocess.check_output("lsblk -o NAME,SIZE", shell=True)
+    contents = urwid.Text(contents_, 'center')
+    device_screen = urwid.Pile([title, urwid.Divider(), contents, ])
+    return device_screen
 
 def main():
     """ Main interface """
     palette = []
 
     header = urwid.Text("Mounter", 'center')
-    body = urwid.Filler(urwid.Text("Hello world.", 'center'), 'middle', min_height=10)
+    body = urwid.Filler(getDeviceScreen(), 'middle', min_height=10)
     footer = urwid.Text("q to quit.", 'right')
 
     ui = urwid.Pile([(25, urwid.Frame(body, header=header, footer=footer))])
