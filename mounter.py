5891bf4f26be530dec1d3168d6a74d2004b8116e
ChanderG <chandergovind@gmail.com>
2016-01-01 17:25:15 +0530

    add infra to transition to second screen
    includes refactors to make this possible

--- mounter.py
+++ mounter.py
@@ -21,27 +21,54 @@ def getDeviceScreen():
     contents = urwid.Text(contents_, 'center')
 
     input_box = urwid.Edit('Enter device name: ', align='center', edit_text='sdb1')
-    enter_button = urwid.Button('Next')
+    enter_button = urwid.Button('Next', on_press=nextScreen)
     button_columns = urwid.Columns([enter_button, ])
     button_line = urwid.Padding(button_columns, 'center', ('relative', 25))
 
     device_screen = urwid.Pile([title, urwid.Divider(), contents, input_box, button_line, ])
     return device_screen
 
+def nextScreen(button):
+    """ Function to go from init screen to mount point selection screen. """
+    global top
+
+    body = urwid.Filler(getMntpntScreen(), 'middle', min_height=10)
+    ui = getScreen(body)
+
+    top.original_widget = ui 
+
 def getMntpntScreen():
     """ Return the mount point screen. """
-    pass
+    global mntpnt_screen
+    if mntpnt_screen != None:
+        return mntpnt_screen
+
+    title = urwid.Text("Select mount point: ", 'center')
+    mntpnt_screen = urwid.Pile([title, urwid.Divider(),])
+    return mntpnt_screen
+
+def getScreen(body):
+    """ Actually creates a screen given a body widget.
+
+    Adds the application level header and footer.
+    """
+    header = urwid.Text("Mounter", 'center')
+    footer = urwid.Text("q to quit.", 'right')
+
+    f = urwid.Frame(body, header=header, footer=footer)
+    ui = urwid.Pile([(25, f)])
+    return ui
 
 def main():
     """ Main interface """
+    global top
     palette = []
 
-    header = urwid.Text("Mounter", 'center')
     body = urwid.Filler(getDeviceScreen(), 'middle', min_height=10)
-    footer = urwid.Text("q to quit.", 'right')
+    ui = getScreen(body)
+    top = urwid.Filler(ui, 'middle')
 
-    ui = urwid.Pile([(25, urwid.Frame(body, header=header, footer=footer))])
-    loop = urwid.MainLoop(urwid.Filler(ui, 'middle'), palette, unhandled_input=handle_top_inputs)
+    loop = urwid.MainLoop(top, palette, unhandled_input=handle_top_inputs)
     loop.run()
 
 if __name__ == "__main__":
