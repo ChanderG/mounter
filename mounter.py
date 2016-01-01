f82fe12075e422d70f46e02425881f853efb955f
ChanderG <chandergovind@gmail.com>
2016-01-01 11:00:53 +0530

    add button to main screen

--- mounter.py
+++ mounter.py
@@ -7,6 +7,7 @@ def handle_top_inputs(key):
         raise urwid.ExitMainLoop()
 
 device_screen = None
+mntpnt_screen = None
 
 def getDeviceScreen():
     """ Return the device screen."""
@@ -18,8 +19,18 @@ def getDeviceScreen():
     title = urwid.Text("Select device: ", 'center')
     contents_ = subprocess.check_output("lsblk -o NAME,SIZE", shell=True)
     contents = urwid.Text(contents_, 'center')
-    device_screen = urwid.Pile([title, urwid.Divider(), contents, ])
+
+    input_box = urwid.Edit('Enter device name: ', align='center', edit_text='sdb1')
+    enter_button = urwid.Button('Next')
+    button_columns = urwid.Columns([enter_button, ])
+    button_line = urwid.Padding(button_columns, 'center', ('relative', 25))
+
+    device_screen = urwid.Pile([title, urwid.Divider(), contents, input_box, button_line, ])
     return device_screen
+
+def getMntpntScreen():
+    """ Return the mount point screen. """
+    pass
 
 def main():
     """ Main interface """
