a8f79d8436d5a8c30b8d7cd20c746b09d4a7f807
ChanderG <chandergovind@gmail.com>
2016-01-02 12:50:44 +0530

    add command verify screen

--- mounter.py
+++ mounter.py
@@ -8,6 +8,7 @@ def handle_top_inputs(key):
 
 device_screen = None
 mntpnt_screen = None
+command_screen = None
 
 def getDeviceScreen():
     """ Return the device screen."""
@@ -48,12 +49,30 @@ def getMntpntScreen():
 
     input_box = urwid.Edit('Enter device name: ', align='center', edit_text='External')
     back_button = urwid.Button('Back', goToScreen, getDeviceScreen)
-    enter_button = urwid.Button('Next', goToScreen, getMntpntScreen)
+    enter_button = urwid.Button('Next', goToScreen, getCommandScreen)
     button_columns = urwid.Columns([back_button, enter_button, ])
     button_line = urwid.Padding(button_columns, 'center', ('relative', 25))
 
     mntpnt_screen = urwid.Pile([title, urwid.Divider(),contents, urwid.Divider(), input_box, button_line, ])
     return mntpnt_screen
+
+def getCommandScreen():
+    """ Return the command screen. """
+    global command_screen
+    if command_screen != None:
+        return command_screen
+
+    title = urwid.Text("Verify command: ", 'center')
+    contents_ = "TODO" # TODO
+    contents = urwid.Text(contents_, 'center')
+
+    back_button = urwid.Button('Back', goToScreen, getMntpntScreen)
+    finish_button = urwid.Button('Finish', goToScreen, getCommandScreen)
+    button_columns = urwid.Columns([back_button, finish_button, ])
+    button_line = urwid.Padding(button_columns, 'center', ('relative', 25))
+
+    command_screen = urwid.Pile([title, urwid.Divider(), contents, urwid.Divider(), button_line, ])
+    return command_screen
 
 def getScreen(body):
     """ Actually creates a screen given a body widget.
